3bf1b21da11ac6d0d90dcbfc98466cdfcbf995c4
jonasht <jhenriquet@outlook.com.br>
2022-12-03 17:02:17 -0300

    add mangodb

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-para instalar os modulos usase, for installing the modules, you have to run
+feito no linux debian 11 com windows Wsl
+it was made in windows wls, debian 11
+
+para instalar os modulos , for installing the modules, running:
 
 npm install 
 no terminal / on terminal
+
+usando mango DB/ using mango DB 
--- src/database/connect.js
+++ src/database/connect.js
@@ -1,16 +1,22 @@
-const mongoose = requiere('mongoose')
+const mongoose = require('mongoose')
+
 
 
 const connectToDatabase = async () => {
-    await mongoose.connect('mongodb+srv://admin:<password>@cluster0.gkv9qrw.mongodb.net/?retryWrites=true&w=majority', (error) => {
+    await mongoose.connect('mongodb+srv://admin:senha@cluster0.gkv9qrw.mongodb.net/?retryWrites=true&w=majority', 
+    (error) => {
+        console.log('username', process.env.MANGO_USERNAME)
+        console.log('pass', process.env.MANGO_PASSWORD)
+
         if (error) {
-            return console.log('erro ao se conectar')
+
+            return console.log('erro ao se conectar: ', error)
 
         }
         return console.log('conexao ao banco de dados realizada com sucesso')
-    })
+    }
+)
 }
 
 
-
-module.exports = connectToDatabase;
+module.exports = connectToDatabase
